e3d44993898408e6196be4b12a95d8cec6b28d33
Nico Angelo Agustin <agustinnico2302@gmail.com>
2025-04-09 19:06:25 +0800

    add while loop

--- MyFirstClass.java
+++ MyFirstClass.java
@@ -1,8 +1,14 @@
 public class MyFirstClass {
     public static void main(String[] args) {
+        boolean isValid = false;
+
         for (int i = 0; i < 5; i++) {
             System.out.println("Hello world");
         }
-        System.out.println(asdasda);
+        System.out.println();
+
+        while (!isValid) {
+            System.out.println("hello");
+        }
     }
 }
